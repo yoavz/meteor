a6c68fcc67851f908a330184f8702e889cec0230
Sashko Stubailo <sashko@mit.edu>
2014-08-19 21:20:23 -0700

    Add the standard jsdoc template

--- scripts/admin/run-jsdoc.sh
+++ scripts/admin/run-jsdoc.sh
@@ -1,2 +1,2 @@
-jsdoc -c conf.json -r packages/accounts-base/ packages/accounts-password/ packages/accounts-oauth/;
+jsdoc -t doc_template -c scripts/conf.json -r packages/accounts-base/ packages/accounts-password/ packages/accounts-oauth/;
 open out/index.html
