1ef513037096d84b980f78279af8586e37335a56
David Glasser <glasser@meteor.com>
2014-10-28 18:35:27 -0700

    Upgrade to pre-release of Faye with proxy support

--- packages/ddp/package.js
+++ packages/ddp/package.js
@@ -12,10 +12,9 @@ Package.describe({
 // will prevent a second copy of faye-websocket from being installed inside
 // sockjs.)
 Npm.depends({
-  // A fork fixing https://github.com/faye/websocket-driver-node/pull/8 (ie
-  // "open from inactive client" errors). Note that sockjs won't use this fork,
-  // but the bug only affects the websocket client, not the server.
-  "faye-websocket": "https://github.com/meteor/faye-websocket-node/tarball/ccc180998b1396093c24d0df7ebc1d199c276552",
+  // A fork pulling in a pre-release of websocket-driver 0.4.0, with proxy
+  // support.
+  "faye-websocket": "https://github.com/meteor/faye-websocket-node/tarball/e1f2fcd0053323eb424f1c1f4124fd59a2413860",
   sockjs: "0.3.9"
 });
 
